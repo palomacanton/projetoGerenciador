b7d1942f77578ecf3c26ac66d93e1ecda0adff7a
palomacanton <palomacanton04@gmail.com>
2023-08-13 18:39:51 -0300

    urls configuradas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FuncionariosComponent } from './components/pages/funcionarios/funcionarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'funcionarios', component: FuncionariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
